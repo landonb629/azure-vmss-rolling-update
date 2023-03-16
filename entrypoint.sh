569c3c07e45631ce0a280c7eab74dd326191387d
Landon Babay <landon.babay@outlook.com>
2023-03-16 15:44:35 -0400

    getting values ready for access

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,11 @@
 
 echo "Resource Group: $1"
 echo "Tenant Id: $2"
+if [[ "$2" == "tenanttester" ]]
+then 
+ echo true
+else 
+ echo false
+fi
 resourceGroupId=$1
 echo "resourceGroupId=$resourceGroupId" >> $GITHUB_OUTPUT
