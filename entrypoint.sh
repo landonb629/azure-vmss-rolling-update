468439e96c5681d56a3e18b42e15fa19b7d5f609
Landon Babay <landon.babay@outlook.com>
2023-03-16 15:47:46 -0400

    WIP: working on getting the entry values setup to take into the script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,4 @@
 
 echo "Resource Group: $1"
 echo "Tenant Id: $2"
-if [[ "$2" == "tenanttester" ]]
-then 
- echo true
-else 
- echo false
-fi
-resourceGroupId=$1
 echo "resourceGroupId=$resourceGroupId" >> $GITHUB_OUTPUT
