3f2d7b56095c84af31153cfd3f147577063e9242
Landon Babay <landon.babay@outlook.com>
2023-03-27 17:29:08 -0400

    adding to the action

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo $scaleSetName
 echo $tenantId 
 echo $clientId 
 echo $clientSecret
-#az login --service-principal --username $CLIENT_ID --password $CLIENT_SECRET --tenant $TENANT_ID
-#az group show -o table
+az login --service-principal --username $clientId --password $clientSecret --tenant $tenantId
+az group show -o table
 
 
