7add155455268d9f18d54e4ed11c1415934daa4f
Landon Babay <landon.babay@outlook.com>
2023-03-30 15:57:51 -0400

    testing locally

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 az login --service-principal --username $AZURE_CLIENT_ID --password $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID 
 loginStatus=$?
-
+echo $loginStatus
 if [[ $loginStatus -eq 1 ]]
 then
   echo "there was an error authenticating to Azure"
@@ -17,6 +18,7 @@ updateStatus=$?
 if [[ $updateStatus -eq 1 ]]
 then 
   echo "Deployment Failed: please check the output for the error message"
+  exit
 elif [[ $status -eq 0 ]]
 then 
   echo "Deployment Completed"
